29a5eb396618d180bd0cbde123de6de177dfc4a3
shaharmeshulam <shahar.mesh@gmail.com>
2021-08-28 16:30:49 +0300

    added checkIfRouteIncludes to AppSearch

--- js/cmps/app-header.jsx
+++ js/cmps/app-header.jsx
@@ -3,13 +3,31 @@ import { AppSearch } from "./app-search.jsx";
 
 const { NavLink, withRouter } = ReactRouterDOM
 
-function _AppHeader() {
-  return (
-    <header className="app-header flex justify-between clickable">
-      <h2><NavLink to="/">AppSus</NavLink></h2>
-      <AppSearch />
-      <AppNav/>
-    </header>
-  )
+class _AppHeader extends React.Component {
+  state = {
+    route: ''
+  }
+
+  componentDidMount() {
+    this.setState({ route: this.props.location.pathname });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevProps.location.pathname !== this.props.location.pathname) {
+      this.setState({ route: this.props.location.pathname });
+    }
+  }
+
+
+  render() {
+    const { route } = this.state;
+    return (
+      <header className="app-header flex justify-between clickable">
+        <h2><NavLink to="/">AppSus</NavLink></h2>
+        <AppSearch route={route}/>
+        <AppNav />
+      </header>
+    )
+  }
 }
 export const AppHeader = withRouter(_AppHeader);
--- js/cmps/app-search.jsx
+++ js/cmps/app-search.jsx
@@ -1,9 +1,21 @@
 import { MailSearch } from "../apps/mail/cmps/mail-search.jsx";
 
 export class AppSearch extends React.Component {
+    state = {
+        route: this.props.route
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if (prevProps.route !== this.props.route) this.setState({ route: this.props.route });
+    }
+
+    checkIfRouteIncludes = (r) => {
+        return this.state.route.includes(r)
+    }
+
     render() {
         return (
-            <MailSearch/>
+            this.checkIfRouteIncludes('mail') && <MailSearch />
         )
     }
 }
